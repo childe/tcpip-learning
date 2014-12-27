268f4189b719e391096e5a7e291e4263e9a17de9
childe <heychilde@gmail.com>
2014-12-28 00:05:07 +0800

    pack and unpack reqeust time without !

--- simpleping.py
+++ simpleping.py
@@ -6,12 +6,12 @@ import struct
 import os
 import time
 import sys
-from .icmp import send_icmp
+from icmp import send_icmp
 
 
 def ping(host):
     pid = os.getpid() & 0xffff
-    data = struct.pack('!d', time.time()) + 48 * 'Q'
+    data = struct.pack('d', time.time()) + 48 * 'Q'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW,
                       socket.getprotobyname('icmp'))
@@ -19,9 +19,10 @@ def ping(host):
     send_icmp(s, host, 8, 0, pid, 0, data)
 
     (content, dst) = s.recvfrom(1024)
-    icmptype, code, checksum, ID, seq, reqtime = struct.unpack(
-        '!bbHHhd', content[
-            20:20+8+8]) #20 IPheader, 8 ICMP header, 8 time.time()
+    icmptype, code, checksum, ID, seq = struct.unpack(
+        '!bbHHh', content[
+            20:20+8])  # 20 IPheader, 8 ICMP header
+    reqtime, = struct.unpack('d', content[28:36])
     print icmptype, code, checksum, ID, seq, reqtime, time.time()-reqtime
 
 
