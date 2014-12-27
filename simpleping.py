3c038e71c2989a75e30a2cc355d7bbbd678822f3
childe <heychilde@gmail.com>
2014-12-28 00:01:16 +0800

    new file:   simpleping.py

---
+++ simpleping.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import struct
+import os
+import time
+import sys
+from .icmp import send_icmp
+
+
+def ping(host):
+    pid = os.getpid() & 0xffff
+    data = struct.pack('!d', time.time()) + 48 * 'Q'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                      socket.getprotobyname('icmp'))
+
+    send_icmp(s, host, 8, 0, pid, 0, data)
+
+    (content, dst) = s.recvfrom(1024)
+    icmptype, code, checksum, ID, seq, reqtime = struct.unpack(
+        '!bbHHhd', content[
+            20:20+8+8]) #20 IPheader, 8 ICMP header, 8 time.time()
+    print icmptype, code, checksum, ID, seq, reqtime, time.time()-reqtime
+
+
+def main():
+    ping(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
